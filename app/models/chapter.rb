c60c5f96e8bd81c1449480d94de93587d8437e66
KMG112 <mattwuertz@gmail.com>
2018-03-07 13:57:31 -0600

    gem install rails

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,13 +1,24 @@
 class Chapter < ApplicationRecord
+    before_destroy :destroy_paths
 
 	has_many :plots
-	has_many :paths, through: :plots, :dependent => :delete_all
+	has_many :paths, through: :plots, dependent: :destroy
 
  	accepts_nested_attributes_for :plots, :paths
     validates :content      , presence: true
     validates :name         , presence: true
 
 
+   private
+
+   def destroy_paths
+   
+     self.paths.each do |t|
+     	t.destroy
+     end  
+
+	
+   end
 
 end
 
--- app/models/path.rb
+++ app/models/path.rb
@@ -1,8 +1,21 @@
 
 class Path < ApplicationRecord
+  before_destroy :destroy_paths
+  
   has_many :plots
-  has_many :chapters, through: :plots
+  has_many :chapters, through: :plots, dependent: :destroy
 
+
+   private
+
+    def destroy_chapters
+	    self.chapters.each do |t|
+	    t.destroy
+    end  
+
+ 	end
 
 end
+
+ 
 
