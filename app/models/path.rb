02131e5db55113900b3db00237df1cb736aa9825
KMG112 <mattwuertz@gmail.com>
2018-03-07 14:51:31 -0600

    destroy

--- app/models/path.rb
+++ app/models/path.rb
@@ -1,19 +1,14 @@
 
 class Path < ApplicationRecord
-  before_destroy :destroy_paths
+  before_destroy :destroy_chapters
   
   has_many :plots
   has_many :chapters, through: :plots, dependent: :destroy
 
 
-   private
 
-    def destroy_chapters
-	    self.chapters.each do |t|
-	    t.destroy
-    end  
 
- 	end
+
 
 end
 
