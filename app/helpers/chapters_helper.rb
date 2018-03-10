dcd3f879df6e1d3e75f7c37747e3e045e494015d
KMG112 <mattwuertz@gmail.com>
2018-03-10 13:56:38 -0600

    Added new  bgs'

--- app/helpers/chapters_helper.rb
+++ app/helpers/chapters_helper.rb
@@ -1,6 +1,6 @@
 module ChaptersHelper
 	def randomized_background_image
-	  images = ["01_bkgrnd.jpg", "02_bkgrnd.jpg", "03_bkgrnd.jpg"]
+	  images = ["01_bkgrnd.jpg", "02_bkgrnd.jpg", "04_bkgrnd.jpg", "05_bkgrnd.jpg", "06_bkgrnd.jpg", "07_bkgrnd.jpg", "08_bkgrnd.jpg", "09_bkgrnd.jpg", "10_bkgrnd.jpg", "11_bkgrnd.jpg", "12_bkgrnd.jpg", "13_bkgrnd.jpg"]
 	  asset_path(images[rand(images.size)])
 	end
 end
